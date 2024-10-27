9f82d631013016ecc59bc1bfda711f1f3505d943
stefanzarembinski <stefan.zarembinski@gmail.com>
2024-10-27 20:46:31 +0100

    update

--- src/kubusia/gasnace_swieczki.py
+++ src/kubusia/gasnace_swieczki.py
@@ -26,20 +26,40 @@ FUTURE_RANGE = 1 * 60
 Y_RANGE = [-2e-3, 2e-3]
 SAVE_SIZE = 2 ** 8
 
+class ExpQuench:
+    def __init__(self, tau=66):
+        self.tau = tau
+
+    def quench(self, t):
+        return math.exp(-math.fabs((t/self.tau)))
+
+class GaussQuench:
+    def __init__(self, tau=66):
+        self.tau = tau
+
+    def quench(self, t):
+        return math.exp(-(t/self.tau) ** 2)
+    
+class FiltrK:
+    def __init__(self, tau=66, exp=0.5):
+        self.tau = tau
+        self.exp = exp
+
+    def quench(self, t):
+        return math.exp(-math.fabs((t/self.tau)) ** self.exp)
+
 FILTERED_VALUE = Savgol_filter(window=50, order=5).filter(VALUE)
-ZANIK = gauss # exp gauss
+
+# ZANIK = ExpQuench(tau=60)
+ZANIK = GaussQuench(tau=60)
+# ZANIK = FiltrK(tau=66, exp=0.5)
+
 CURRENT_FIG_DIR = path.normpath(path.join(DIRNAME,'../../obrazki_kubusia/current'))
 FUTURE_FIG_DIR = path.normpath(path.join(DIRNAME,'../../obrazki_kubusia/future'))
 TIMESTRING = '%y-%m-%d_%H-%M'
 # import pdb; pdb.set_trace()
 
-SKALA_ZANIKU_GAUSS = 2 * 60.0 
-def gauss(t):
-    return math.exp(-(t/SKALA_ZANIKU_GAUSS) ** 2)
 
-SKALA_ZANIKU_K = 60.0
-def filtr_k(t, exponent=0.5):
-    return math.exp(-math.fabs((t/SKALA_ZANIKU_K)) ** exponent)
 
 class Plot:
     def __init__(self, plotter=plt, axis_off=False):
@@ -69,7 +89,7 @@ class Plot:
             plt.close()
 
 class PlotCurrent(Plot):
-    def __init__(self, time_start=0, plotter=plt, zanik_class=gauss, axis_off=False):
+    def __init__(self, time_start=0, plotter=plt, zanik_class=ZANIK, axis_off=False):
         super().__init__(plotter=plotter, axis_off=axis_off)
 
         filtered_value = FILTERED_VALUE[time_start: time_start + CURRENT_RANGE]
@@ -78,7 +98,7 @@ class PlotCurrent(Plot):
         self.time_count = np.array([i for i in range(CURRENT_RANGE)], dtype='float64')
         self.time_count = self.time_count - self.time_count[-1]
 
-        self.zanik = np.array([filtered_value[i] * zanik_class(self.time_count[i]) for i in range(len(self.time_count))])
+        self.zanik = np.array([filtered_value[i] * zanik_class.quench(self.time_count[i]) for i in range(len(self.time_count))])
         self.timestamp = TIMESTAMP[time_start + CURRENT_RANGE]
         # import pdb; pdb.set_trace()
         
@@ -105,14 +125,44 @@ class PlotFuture(Plot):
         self.off()
         self.plotter.axis([0, CURRENT_RANGE, Y_RANGE[0], Y_RANGE[1]])
 
+class PlotBoth(Plot):
+    def __init__(self, time_start=0, plotter=plt, zanik_class=ZANIK, axis_off=False):
+        super().__init__(plotter=plotter, axis_off=axis_off)
+
+        filtered_value = FILTERED_VALUE[time_start: time_start + CURRENT_RANGE]
+        filtered_value = filtered_value - filtered_value[-1]
+
+        self.time_count_l = np.array([i for i in range(CURRENT_RANGE)], dtype='float64')
+        self.time_count_l = self.time_count_l - self.time_count_l[-1]
+
+        self.zanik = np.array([filtered_value[i] * zanik_class.quench(self.time_count_l[i]) for i in range(len(self.time_count_l))])
+        
+        value = VALUE[time_start + CURRENT_RANGE: time_start + CURRENT_RANGE + FUTURE_RANGE]
+        self.value = value - value[0]
+
+        self.time_count_r = np.array([i for i in range(FUTURE_RANGE)], dtype='float64')
+        
+        self.timestamp = TIMESTAMP[time_start + CURRENT_RANGE]
+        # import pdb; pdb.set_trace()
+        
+    def plot(self):
+        self.plotter.plot(self.time_count_r, self.value, color='black', linewidth=1)
+
+        self.plotter.plot(self.time_count_l, self.zanik, color='black', linewidth=0.0)
+        self.plotter.fill_between(self.time_count_l, self.zanik, where=((self.zanik < 0)), color='blue')
+        self.plotter.fill_between(self.time_count_l, self.zanik, where=((self.zanik > 0)), color='red')
+        self.off()
+        self.plotter.axis([-CURRENT_RANGE, FUTURE_RANGE, Y_RANGE[0], Y_RANGE[1]])
+
+
 def dwa_obrazki(time_start=0, axis_off=False):
-    fig, (current, future) = plt.subplots(1, 2, figsize=(10, 5))
+    fig, (current, both) = plt.subplots(1, 2, figsize=(10, 5))
 
     plot = PlotCurrent(time_start, current, axis_off=axis_off)
     plot.plot()
     print(plot.file_name(CURRENT_FIG_DIR))
 
-    plot = PlotFuture(time_start, future, axis_off=axis_off)
+    plot = PlotBoth(time_start, both, axis_off=axis_off)
     plot.plot()
     print(plot.file_name(FUTURE_FIG_DIR))
 
@@ -128,8 +178,8 @@ def save(time_start=0):
     # plt.close()
 
 def main():
-    # dwa_obrazki(time_start=20, axis_off=False)
-    save()
+    dwa_obrazki(time_start=20, axis_off=False)
+    # save()
 
 
 if __name__ == "__main__":
