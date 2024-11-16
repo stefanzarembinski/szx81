37e8f8582d6fb4485367f7f080b8fb9f4f4d534e
stefanzarembinski <stefan.zarembinski@gmail.com>
2024-11-16 21:44:46 +0100

    update

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,6 +2,7 @@ import collections
 from os import path
 import math
 import numpy as np
+import pickle
 np.set_printoptions(formatter={'float_kind':"{:-.3e}".format})
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -10,22 +11,90 @@ from core import config
 import piecewise_fit as ls
 import test_data as td
 
+
+
 class Tokenizer:
     number_pieces = 10
     window = 120
     margin = 0.05
-    time_qizer = None
-    value_qizer = None
-    temperature_qizer = None
+    level_count = 8
+    time_levels = None
+    value_levels = None
+    temperature_levels = None
     filter = co.Savgol_filter(window=50, order=5)
     none_word = '0000'
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
         self.clazz = None
         self.time_part = None
         self.value_part = None
         self.temp_part = None
+
+    def save_data(data):
+        with open(path.join(
+                co.DATA_STORE, 
+                f'data_{Tokenizer.window}_{Tokenizer.number_pieces}.pkl'), 'wb') as f:
+            pickle.dump(data, f)
+
+    def get_data_from_file(set_levels=True):  
+        try:   
+            with open(
+                path.join(
+                    co.DATA_STORE, 
+                    f'data_{Tokenizer.window}_{Tokenizer.number_pieces}.pkl'), 'rb') as f:
+                data = pickle.load(f)
+        except:
+            return None
+        return data
+    
+    def save_levels(time_stat, value_stat, temperature_stat, set_levels=False):
+        """Derives quantization levels from data statistics and save them 
+        to file.
+
+        Parameters
+        ----------
+        time_stat, value_stat, temperature_stat : Data statistics as returned 
+            from :py:func:`Tokenizer.get_statistics`
+
+        set_levels : Write levels to Tokenizer definitions
+
+        Returns
+        -------
+        time_level, value_level, temperature_level : Quantization levels.
+        """
+        time_level, value_level, temperature_level = Tokenizer.set_quantization_levels(
+            time_stat, value_stat, temperature_stat, set_levels)
+        
+        with open(path.join(
+                co.DATA_STORE, 
+                f'levels_{Tokenizer.window}_{Tokenizer.number_pieces}.pkl'), 'wb') as f:
+            pickle.dump((time_level, value_level, temperature_level), f)
+
+        return time_level, value_level, temperature_level
+
+    def get_levels_from_file(set_levels=True):
+        """Loads quantization levels from file
+
+        Parameters
+        ----------
+        set : Write levels to Tokenizer definitions
+
+        Returns
+        -------
+        time_level, value_level, temperature_level : Quantization levels.
+        """     
+        with open(
+            path.join(
+                co.DATA_STORE, 
+                f'levels_{Tokenizer.window}_{Tokenizer.number_pieces}.pkl'), 'rb') as f:
+            time_levels, value_levels, temperature_levels = pickle.load(f)
+
+        if set_levels:
+            Tokenizer.time_levels = time_levels
+            Tokenizer.value_levels = value_levels
+            Tokenizer.temperature_levels = temperature_levels
+        
+        return time_levels, value_levels, temperature_levels
 
     def save_story(whole_story):
         whole_story = Tokenizer.get_sentence_str(whole_story)
@@ -62,20 +131,29 @@ class Tokenizer:
                 words.append(eval(line[:-1]))
         return words
     
-    def set_quantization_limits(self):
-        def limit(a):
-            hist, bin_edges = np.histogram(a, bins=20, density=True)
-            int = np.array([np.sum((hist * np.diff(bin_edges))[:k]) for k in range(len(hist))])
-            return bin_edges[np.argmax(int > 1 - Tokenizer.margin)]
-        
+    def get_statistics(data, bins=20):
+        """Provides statistics of the token attributes: `time`, `value`, `temperature`.
+
+        Parameters
+        ----------
+        data : List of tuples, each consisted of a timestamp, tuple of two 
+            (ASK and BID) candles, and volume.
+        bins : The number of histogram bins.
+
+        Returns
+        -------- 
+        time_stat : tuple ``(hist, bin_edges)`` for 'time' statistics.
+        value_stat : tuple ``(hist, bin_edges)`` for 'value' statistics.
+        temperature_stat : tuple ``(hist, bin_edges)`` for 'temperature' statistics.
+        """        
         shift = 0
         time_set = []
         value_set = []
         temperature_set = []
 
-        while shift + Tokenizer.window < len(self.data):
+        while shift + Tokenizer.window < len(data):
             clazz = ls.piecewise(
-                value=[(_[1][0][0] + _[1][0][1]) / 2 for _ in self.data[shift: shift + Tokenizer.window]], 
+                value=[(_[1][0][0] + _[1][0][1]) / 2 for _ in data[shift: shift + Tokenizer.window]], 
                 filter=Tokenizer.filter, 
                 number_pieces=Tokenizer.number_pieces)
             xk, yk = clazz.knots()
@@ -84,121 +162,185 @@ class Tokenizer:
             temperature_set.extend(clazz.temperature())
             shift += Tokenizer.window
         
-        Tokenizer.time_qizer =  Quantizator(limit(time_set))
-        Tokenizer.value_qizer = Quantizator(limit(value_set))
+        return (
+            (np.histogram(time_set, bins=bins, density=True)),
+            (np.histogram(value_set, bins=bins, density=True)),
+            (np.histogram(temperature_set, bins=bins, density=True)),
+        ) 
+    
+    def round_sig(x, sig=3):
+        x_ = math.fabs(x)
+        retval = round(x_, sig-int(math.floor(math.log10(abs(x_))))-1)
+        if x < 0: retval *= -1
+        return retval
+
+    def set_quantization_levels(time_stat, value_stat, temperature_stat, set=True):
+        def histogram(hist, bin_edges, level_count):
+            hist_cum = hist.cumsum()
+            hist_cum = hist_cum / hist_cum[-1]
+            
+            level_dist = 1 / (level_count + 1)
+            levels = []
+            for i in range(level_count):
+                index = (np.abs(hist_cum - (i + 1) * level_dist).argmin())
+                levels.append(bin_edges[index])
+
+            return [Tokenizer.round_sig(_) for _ in levels]
         
-        Tokenizer.temperature_qizer = Quantizator(limit(temperature_set))
+        time_levels = histogram(*time_stat, Tokenizer.level_count)
+        value_levels = histogram(*value_stat, 2 * Tokenizer.level_count)
+        temperature_levels = histogram(*temperature_stat, Tokenizer.level_count)
+
+        if set:
+            Tokenizer.time_levels = time_levels
+            Tokenizer.value_levels = value_levels
+            Tokenizer.temperature_levels = temperature_levels
+
+        return time_levels, value_levels, temperature_levels
+ 
+
+    def quantize(x, levels):
+        """Returns given array quantized according to given levels.
+
+        Parameters
+        ----------
+        x : Array of numbers to be quantized.
+        levels : Array of quantization values.
+
+        Returns
+        -------
+        x_quant : Quantized input array. 
+        """
+        def do(x):
+            if x <= levels[0]:
+                return levels[0]
+            if x >= levels[-1]:
+                return levels[-1]
+
+            for i in range(1, len(levels)):
+                if x < levels[i]:
+                    mean = (levels[i-1] + levels[i]) * .5
+                    if x <= mean: return levels[i-1]
+                    return levels[i]
+        return np.array([do(_) for _ in x])
 
     def get_sentence(self, data):
-        """Converts a short chunk of data into tokens.
+        """Converts a short chunk of data into tokens. but only if the quantization
+        limits are already  
 
-        :param data: List of tuples, each consisted of a timestamp, tuple of two 
+        Parameters
+        ----------
+        data : List of tuples, each consisted of a timestamp, tuple of two 
         (ASK and BID) candles, and volume.
-        :return: List of tuples, each having the token and its timestamp.
+        
+        Returns
+        -------
+        token_timestamp : List of tuples, each having the token and its timestamp.
+
+        Notes
+        -----
+        Quantum levels has to be established with :py:func:`Tokenizer.set_quantization_levels' 
+        function.
 
         Takes a list of tuples, each consisted of a timestamp, tuple of two 
-        (ASK and BID) candles, and volume e.g: 
-        `(
-            1672841640.0, 
+        (ASK and BID) candles, and volume e.g:
+
+        ``` 
             (
-                [-0.001587, -0.001267, -0.001737, -0.001437], 
-                [-0.001637, -0.001317, -0.001787, -0.0014773]
-            ), 
-            1055900.0244
-        )`.
+                1672841640.0, 
+                (
+                    [-0.001587, -0.001267, -0.001737, -0.001437], 
+                    [-0.001637, -0.001317, -0.001787, -0.0014773]
+                ), 
+                1055900.0244
+            )
+        ```
 
         Produces a list of tuples, each having the token and its timestamp e.g:
-        `(
+
+        ```
             (
-                19.14, 4.83e-05, -0.00727
-            ), 
-            1672782932.5137246
-        )'
+                (
+                    19.14, 4.83e-05, -0.00727
+                ), 
+                1672782932.5137246
+            )
+        ```
+        
         """
+        
         self.clazz = ls.piecewise(
             value=[(_[1][0][0] + _[1][0][1]) / 2 for _ in data], 
             filter=Tokenizer.filter, number_pieces=Tokenizer.number_pieces)
-        # import pdb; pdb.set_trace()
+        
         time_set, value_set = self.clazz.knots()
         self.time_part = [time_set[i] - time_set[i-1] for i in range(1, len(time_set))]
-        time_qu = Tokenizer.time_qizer.quantize(self.time_part)
+        time_qu = Tokenizer.quantize(self.time_part, Tokenizer.time_levels)
         time0 = data[0][0]
         time_qu_abs = np.cumsum(time_qu) * config.PERIOD + time0
 
         self.value_part = [value_set[i] for i in range(1, len(value_set))]
-        self.temp_part = self.clazz.temperature()
-        value_qu = Tokenizer.value_qizer.quantize(self.value_part)
-        temp_qu = Tokenizer.temperature_qizer.quantize(self.temp_part)
+        import pdb; pdb.set_trace()
+        value_qu = Tokenizer.quantize(self.value_part, Tokenizer.value_levels)
         
-        token = [((time_qu[i], temp_qu[i], value_qu[i]), time_qu_abs[i]) for i in range(len(time_qu))]
+        self.temp_part = self.clazz.temperature()
+        temp_qu = Tokenizer.quantize(self.temp_part, Tokenizer.temperature_levels)
+        
+        token = [((round(time_qu[i], 1), value_qu[i], temp_qu[i]), 
+                  round(time_qu_abs[i])) for i in range(len(time_qu))]
         return token
     
-    def get_story(self, data, save=False, window=120):
+    def get_whole_story(data, save=False, window=120):
         """Converts data into tokens.
 
-        :param data: List of tuples, each consisted of a timestamp, tuple of two 
-        (ASK and BID) candles, and volume.
-        :param save: Optional, if set, the result is saved to file.
-        :param window: Length of the data chunk tokenized in a single batch of
+        Parameters
+        ----------
+        data : List of tuples, each consisted of a timestamp, tuple of two 
+            (ASK and BID) candles, and volume.
+            :param save: Optional, if set, the result is saved to file.
+        window : Length of the data chunk tokenized in a single batch of
         conversion.
-        :return: List of tuples, each having the token and its timestamp.
 
+        Returns
+        -------
+        token_timeshot : List of tuples, each having the token and its timestamp.
+
+        Notes
+        -----
         Takes a list of tuples, each consisted of a timestamp, tuple of two 
-        (ASK and BID) candles, and volume e.g: 
-        `(
-            1672841640.0, 
+        (ASK and BID) candles, and volume e.g:
+        
             (
-                [-0.001587, -0.001267, -0.001737, -0.001437], 
-                [-0.001637, -0.001317, -0.001787, -0.0014773]
-            ), 
-            1055900.0244
-        )`.
+                1672841640.0, 
+                (
+                    [-0.001587, -0.001267, -0.001737, -0.001437], 
+                    [-0.001637, -0.001317, -0.001787, -0.0014773]
+                ), 
+                1055900.0244
+            )
 
         Produces a list of tuples, each having the token and its timestamp e.g:
-        `(
+
             (
-                19.14, 4.83e-05, -0.00727
-            ), 
-            1672782932.5137246
-        )'
+                (
+                    19.14, 4.83e-05, -0.00727
+                ), 
+                1672782932.5137246
+            )
+
         """
+        tokenizer = Tokenizer(data)
+        Tokenizer.set_quantization_levels(*Tokenizer.get_statistics(data, bins=50)) 
+
         shift = 0
         token_plus = []
         data = td.DATA
         while shift + window < len(data):
-                token_plus.extend(self.get_sentence(data[shift: shift + window]))
+                token_plus.extend(tokenizer.get_sentence(data[shift: shift + window]))
                 shift += window
         if save:
             Tokenizer.save_story(token_plus)
         return token_plus
-
-class Quantizator:
-    def __init__(self, limit, level_count=8):
-        self.levels = np.array([(lambda x: (2 ** x))(x) for x in range(level_count)])
-        self.levels = self.levels * (limit / self.levels[-1])
-
-    def approx(self, x):
-        x_ = math.fabs(x)
-        if x_ <= self.levels[0]:
-            retval = self.levels[0]
-            if x < 0 : retval *= -1
-            return retval
-        if x_ >= self.levels[-1]:
-            retval = len(self.levels) - 1
-            if x < 0 : retval += 8
-            return retval
-
-        for i in range(1, len(self.levels)):
-            if x_ < self.levels[i]:
-                mean = (self.levels[i-1] * self.levels[i]) ** .5
-                if x_ <= mean: retval = self.levels[i-1]
-                if x_ >= mean: retval = self.levels[i]
-                if x < 0: retval *= -1
-                return retval
-            
-    def quantize(self, x):
-        return np.array([self.approx(_) for _ in x])
-
 
 def test_temperature():
     window = 120
@@ -220,13 +362,14 @@ def test_temperature():
 
 
 def test_quantization():
-    tokenizer = Tokenizer(td.VALUE)
-    tokenizer.set_quantization_limits()
+    tokenizer = Tokenizer(td.Data)
+    Tokenizer.set_quantization_levels(Tokenizer.get_statistics(td.DATA, bins=100))
+
     shift = 0
     window = 120
     time_value_temp = []
-    while shift + window < len(td.VALUE):
-        time_value_temp.extend(tokenizer.get_sentence(td.VALUE[shift: shift + window]))
+    while shift + window < len(td.DATA):
+        time_value_temp.extend(tokenizer.get_sentence(td.DATA[shift: shift + window]))
         shift += window
     # import pdb; pdb.set_trace()
     print(set(Tokenizer.get_sentence_str(time_value_temp)))
@@ -240,52 +383,61 @@ def test():
     set_test_data(
         data_count=10000, 
         moving_av=True)
-
-    tokenizer = Tokenizer(td.DATA)
-    tokenizer.set_quantization_limits() 
-    data = td.DATA
-    token_plus = tokenizer.get_story(data)
     
-    for i in range(3):
-        print(f'token[{i}]:\n {token_plus[i]}')
+    DATA = td.DATA
+    tokenizer = Tokenizer()
+    print('Tokenizer.temperature_levels: ', Tokenizer.temperature_levels)
+    Tokenizer.get_levels_from_file(set_levels=True)
+
+    shift = 1000
+    window = 120
+    tokenizer.get_sentence(DATA[shift: shift + window])
+
+
+
+    # data = td.DATA
+    # token_plus = Tokenizer.get_whole_story(data)
     
-    import pdb; pdb.set_trace()
-    token = [_[0] for _ in token_plus]
+    # for i in range(300, 305):
+    #     print(f'token[{i}]:\n {token_plus[i]}')
+    
+    # import pdb; pdb.set_trace()
+    # token = [_[0] for _ in token_plus]
 
-    def plot_token(token_plus):
+    # def plot_token(token_plus):
 
-        begin_index = next((i for i, t in enumerate(data) if t[0] > token_plus[0][1]), -1)
-        end_index = next((i for i, t in enumerate(data) if t[0] > token_plus[0][1]), -1)
-        nonlocal previous
-        previous = None
-        def decode_token(tp, x):
-            if (diff := (previous[0][0] - tp[0][0])) == 0:
-                return tp[1]
+    #     begin_index = next((i for i, t in enumerate(data) if t[0] > token_plus[0][1]), -1)
+    #     end_index = next((i for i, t in enumerate(data) if t[0] > token_plus[0][1]), -1)
+    #     nonlocal previous
+    #     previous = None
+    #     def decode_token(tp, x):
+    #         if (diff := (previous[0][0] - tp[0][0])) == 0:
+    #             return tp[1]
             
-            previous = tp
-            return (tp[0][2] - previous[0][2]) / diff * x 
+    #         previous = tp
+    #         return (tp[0][2] - previous[0][2]) / diff * x 
            
-        first = True
-        time_tp = []
-        value_tp = []
-        time = data[begin_index][1]
-        for tp in token_plus:
-            if first:
-                previous = tp
-                first = False
+    #     first = True
+    #     time_tp = []
+    #     value_tp = []
+    #     time = data[begin_index][1]
+    #     for tp in token_plus:
+    #         if first:
+    #             previous = tp
+    #             first = False
             
-            while time < tp[0]:
-                pass
+    #         while time < tp[0]:
+    #             pass
 
 
-    plot_token(token_plus)
+    # plot_token(token_plus)
 
 
 
-    current = ((2.3935439343241414, 4.836509748507084e-05, -0.007276000929348797), 1672781783.612636)
-    for tp in token_plus:
-        previous = current
-        current = tp
+    # current = ((2.3935439343241414, 4.836509748507084e-05, -0.007276000929348797), 1672781783.612636)
+    # for tp in token_plus:
+    #     previous = current
+    #     current = tp
 
 
 
