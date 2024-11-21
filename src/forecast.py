98ca8a509ec239e7111b5eaf67e817d0515f9f38
stefanzarembinski <stefan.zarembinski@gmail.com>
2024-11-21 19:55:48 +0100

    update

--- src/forecast.py
+++ src/forecast.py
@@ -8,7 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import core as co
 from core import config
 import piecewise_fit as ls
-import hist_data as td
+import hist_data as hd
 
 class Forecast:
     PIP = 1e-4 # XTB min spread is 0.5 pip for STANDARD account. Market spread min. is 0.1 pip
@@ -47,6 +47,12 @@ min ask: {min(self.ask()) / Forecast.PIP:.1f}
 diff: {(self.bid()[0] - min(self.ask())) / Forecast.PIP:.1f}
 ''' 
         def set_direction(direction, opposite):
+            """Called when trend direction is determined. Then sets statistics.
+            Parameters:
+            -----------
+            direction : lambda function either `self.ask` or `self.bid`.
+            opposite : lambda function other then set with `direction`.
+            """
             if direction is None:
                 return
             self.direction = direction
@@ -57,19 +63,21 @@ diff: {(self.bid()[0] - min(self.ask())) / Forecast.PIP:.1f}
             self.end = direction()[-1]
             self.begin_price = opposite()[0]
             
-            self.panic = 0
+            self.panic = None
             if direction == self.bid:
-                self.min_end_price = self.begin_price - self.threshold                
+                self.min_end_price = self.begin_price - self.threshold
+                self.threshold_level = self.ask()[0] + self.threshold
                 self.panic_level = opposite()[0] - panic_level
-                m = min((direction() < self.panic_level) * direction()) 
-                if m < 0:
-                    self.panic = math.fabs(m - self.panic_level)
+                self.sell_time = np.argmax(self.bid() > self.threshold_level)               
+                # import pdb; pdb.set_trace()
+                self.panic = -(min(direction()[:self.sell_time]) - self.panic_level)
             elif direction == self.ask:
                 self.min_end_price = self.begin_price + self.threshold
+                self.threshold_level = self.begin_price - self.threshold
                 self.panic_level = opposite()[0] + panic_level
-                m = max((direction() > self.panic_level) * direction())
-                if m > 0:
-                    self.panic = math.fabs( - self.panic_level)
+                self.buy_time = np.argmax(self.ask() < self.threshold_level)
+                # import pdb; pdb.set_trace()
+                self.panic = max(direction()[:self.buy_time]) - self.panic_level
             
             difference = self.min_end_price - self.begin_price
             self.min_profit = - difference
@@ -103,7 +111,12 @@ diff: {(self.bid()[0] - min(self.ask())) / Forecast.PIP:.1f}
             direction = 'ask: sell-buy'
         str += f'direction: {direction}\n'
         str += f'min profit [PIP]: {self.min_profit / Forecast.PIP:.2f}\n'
-        str += f'panic value [PIP]: {self.panic / Forecast.PIP:.2f}\n'
+        # import pdb; pdb.set_trace()
+        if self.panic is not None:
+            str += f'panic value [PIP]: {self.panic / Forecast.PIP:.2f}' \
+                + (' - no panic\n' if self.panic < 0 else '\n')
+        else:
+            str += 'no panic'
         return str
     
     def plot(self):
@@ -111,15 +124,17 @@ diff: {(self.bid()[0] - min(self.ask())) / Forecast.PIP:.1f}
         plt.plot(cndl_count, self.ask() / Forecast.PIP, label='ask')
         plt.plot(cndl_count, self.bid() / Forecast.PIP, label='bid')
         if self.direction == self.bid:
-            threshold_level = self.ask()[0] + self.threshold
-            plt.scatter([cndl_count[0]], self.ask()[0] / Forecast.PIP, label='buy point')
-            sell = np.argmax(self.bid() > threshold_level)
-            plt.scatter([cndl_count[sell]], self.bid()[sell] / Forecast.PIP, label='sell point')
+            plt.scatter([cndl_count[0]], self.ask()[0] / Forecast.PIP, 
+                        marker='x', linewidths=10, label='buy point')
+            plt.scatter(
+                [cndl_count[self.sell_time]], 
+                self.bid()[self.sell_time] / Forecast.PIP, 
+                marker='x', linewidths=10, label='sell point')
 
             plt.hlines(self.panic_level / Forecast.PIP, 
                        cndl_count[0], cndl_count[-1], color='red', label='panic level',
                        linestyle='--')
-            plt.hlines(threshold_level / Forecast.PIP, 
+            plt.hlines(self.threshold_level / Forecast.PIP, 
                        cndl_count[0], cndl_count[-1], label='min sell level')
             
             if not self.plotall:
@@ -127,15 +142,17 @@ diff: {(self.bid()[0] - min(self.ask())) / Forecast.PIP:.1f}
                     -1e-4 / Forecast.PIP, 2 * self.threshold / Forecast.PIP])
 
         elif self.direction == self.ask:
-            threshold_level = self.bid()[0] - self.threshold
-            plt.scatter([cndl_count[0]], self.bid()[0] / Forecast.PIP, label='sell point')
-            buy = np.argmax(self.ask() < threshold_level)
-            plt.scatter([cndl_count[buy]], self.ask()[buy] / Forecast.PIP, label='buy point')
+            plt.scatter([cndl_count[0]], self.bid()[0] / Forecast.PIP, 
+                        marker='x', linewidths=10, label='sell point')
+            plt.scatter(
+                [cndl_count[self.buy_time]], 
+                self.ask()[self.buy_time] / Forecast.PIP, 
+                marker='x', linewidths=10, label='buy point')
             
             plt.hlines(self.panic_level / Forecast.PIP, 
                        cndl_count[0], cndl_count[-1], color='red', label='panic level',
                        linestyle='--')
-            plt.hlines(threshold_level / Forecast.PIP, 
+            plt.hlines(self.threshold_level / Forecast.PIP, 
                        cndl_count[0], cndl_count[-1], label='min buy level')
             
             if not self.plotall:
@@ -148,19 +165,19 @@ diff: {(self.bid()[0] - min(self.ask())) / Forecast.PIP:.1f}
         plt.show()
 
 def test_forecast():
-    forecast_window = config.forecast_window
-    forecast_window = 30
-    forecast_threshold = 2e-4 + 1e-4 # 1 pip for spread    
+    FORECAST_WINDOW = 30
+    FORECAST_THRESHOLD = 2e-4 + 1e-4 # spread
 
-    shift = 1600
+    shift = 1500
     forecast = Forecast(
-        td.VALUE[shift: forecast_window + shift], 
-        threshold=forecast_threshold, plotall=False)
+        hd.VALUE[shift: FORECAST_WINDOW + shift], 
+        threshold=FORECAST_THRESHOLD)
     print(forecast)
-    forecast.plot() 
+    forecast.plot()
 
 def main():
-    td.set_test_data(data_count=20000, moving_av=True)
+    hd.set_hist_data(data_count=None, moving_av=True)
+    test_forecast()
 
 if __name__ == "__main__":
     main()
