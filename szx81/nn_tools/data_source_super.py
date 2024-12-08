651ef271d50786d14f2b7c75db26a7f0deb074d3
stefanzarembinski <stefan.zarembinski@gmail.com>
2024-12-08 22:19:06 +0100

    update

--- szx81/nn_tools/data_source_super.py
+++ szx81/nn_tools/data_source_super.py
@@ -9,80 +9,112 @@ hd.set_hist_data(data_count=None)
 
 class DataSource:
     def __init__(self, data, scalers, **kwargs):
+        """ The ``get_data`` method returns the given count of data object 
+        needed for composing feature and target inputs of an NN model.
+
+        Parameters
+        ----------
+        data : Array of historical data available.
+        scalers: Array of NN model inputs scalers.
+        """
         
         self.data = list(data)
         self.scalers = scalers
         self.feature_count = 2
         self.end_index = None
+        self.begin_index= None
+        self.feature_data_count = None
         self.data_count = None
         self.features = None
         self.targets = None
         self.indexes = None
- 
-        self.fit_data(**kwargs)
+        self.opens_hist = None
+        self.volumes_hist = None
+        self.indexes_hist = None
+        self.kwargs = kwargs
+        self.scaler_opens = MinMaxScaler()
+        self.scaler_volumes = MinMaxScaler()
+
+        self.scaler_opens.fit(np.array(
+            [(_[1][0][0] + _[1][1][0]) / 2 for _ in data]).reshape(-1, 1))
+        self.scaler_volumes.fit(
+            self.log_volume(np.array([_[2] for _ in data]).reshape(-1, 1)))
+
+        self.fit_data()
 
     def fit_data(self):
-        return
-
-    def feature_names(self):
-        return [str(i) for i in range(20)]
-        # Change accordingly
+        raise NotImplementedError()
     
+    def feature_names(self):
+        raise NotImplementedError()
+        return [str(i) for i in range(20)]
+    
+    def raw_data_begin(self):
+        """Calculates ``beginning index of historical data needed and returns 
+        it.
+        """
+        raise NotImplementedError()
+        return self.begin_index
+    
+    def get_data__(self):
+        raise NotImplementedError()
+        return (
+            self.features, 
+            self.targets, 
+            self.indexes)
+
     def target_names(self):
         return [str(i) for i in range(20)]
         # Change accordingly   
 
+    def plot_ds(self, plt):
+        raise NotImplementedError()
+    
+    def log_volume(self, volume):
+        return np.log(volume + 10)
+    
     def fit(self, data):
         for i in range(len(data)):
             if data[i] is not None:
-                data[i] = self.scalers[i].fit(data[i])
+                data[i] = self.scalers[i].fit(data[i].reshape(-1, 1))
         return data
 
     def fit_transform(self, data):
         for i in range(len(data)):
             if data[i] is not None:
-                data[i] = self.scalers[i].fit_transform(data[i])
+                data[i] = self.scalers[i].fit_transform(data[i].reshape(-1, 1))
         return data
     
     def transform(self, data, index=None):
         if index is None:
             for i in range(len(data)):
                 if data[i] is not None:
-                    data[i] = self.scalers[i].transform(data[i])
+                    data[i] = self.scalers[i].transform(data[i].reshape(-1, 1))
         else:
-            data = self.scalers[index].transform(data)
+            data = self.scalers[index].transform(data.reshape(-1, 1))
         return data
     
     def inverse_transform(self, data, index=None):
         if index is None:
             for i in range(len(data)):
                 if data[i] is not None:
-                    data[i] = self.scalers[i].inverse_x(data[i])
+                    data[i] = self.scalers[i].inverse_x(data[i].reshape(-1, 1))
         else:
-            data = self.scalers[index].inverse_x(data)        
+            data = self.scalers[index].inverse_x(data.reshape(-1, 1))        
         return data
     
     def len(self):
         return len(self.indexes)
     
-    def get_data__(self):
-        # Change accordingly   
-        return (
-            self.features, 
-            self.targets, 
-            self.indexes)
-    
     def get_data(self, end_index, data_count, future_count):
         """
-        Returns amount of data, typically called by a ``Context Sequencer`` 
-        or ``Predictor``.
+        Returns the given count of data object needed for composing feature and 
+        target inputs of an NN model.
 
         Parameters:
         -----------
         end_index : Index of oldest historical data used, including targeting.
-        data_count : Index of the earliest historical data used is 
-            `end_index - data_count` (possibly lowered in order to meet 
-            constrictions.)
+        data_count : Number of data units needed for imputing a NN model.
         future_count : Historical data index difference between training
             and prediction data
 
@@ -90,42 +122,37 @@ class DataSource:
         --------
 
         (features, targets, indexes) : Tuple of numpy arrays containing
-            data relevant (yet not formatted) for nn modeling.
+            data ordered.
         """
-        self.end_index = end_index
+        self.end_index = end_index + 1
         self.data_count = data_count
         self.future_count = future_count
+        self.begin_index= self.raw_data_begin()
 
-        feature_data_count = self.data_count - self.feature_count
-
-        if feature_data_count % self.step != 0:
-            feature_data_count = (feature_data_count // self.step + 1) \
-                * self.step
-        self.begin = self.end_index - feature_data_count - future_count
-        if self.begin + 1 < 0 or self.end_index + 1 > len(self.data):
+        if self.begin_index+ 1 < 0 or self.end_index + 1 > len(self.data):
             raise Exception(f''' 
 ERROR
 The wanted data range is 
-({self.begin + 1}, {self.end_index + 1})
+({self.begin_index+ 1}, {self.end_index + 1})
 while the maximal is
 ({0}, {len(self.data)})
 ''')
+        data = self.data[self.begin_index: self.end_index]
+        self.opens_hist = np.array(
+            [(_[1][0][0] + _[1][1][0]) / 2 for _ in data])
+        self.volumes_hist = self.log_volume(np.array([_[2] for _ in data]))
+        self.indexes_hist = np.array(
+            [i for i in range(self.begin_index, self.end_index)])
 
         return self.get_data__()
         
     def report(self, verbose=False):
         index_count = self.indexes[-1] - self.indexes[0]
         print(f'''
+REPORT: 
 wanted end index: ({self.end_index})
 wanted data count {self.data_count}
-index range: ({self.indexes[0]}, {self.indexes[-1]})
-index count: {self.indexes[-1] - self.indexes[0]}''')
-        if index_count != self.data_count:
-            print('Real data count is adjusted to constrictions.')
-        else:
-            print()
-        if verbose:
-            print(f'''
+hist. data index range: ({self.indexes_hist[0]}, {self.indexes_hist[-1]})
 features:
 {self.features[:3]}
 targets:
@@ -134,15 +161,19 @@ indexes:
 {self.indexes[:3]}
 ''')
 
-    def plot(self):
-        targets = self.targets.transpose()
-        for i in range(len(targets)):
-            plt.plot(self.indexes, targets[i], marker='o', 
-                     label=self.target_names()[i])
-        features = self.features.transpose()
-        for i in range(len(features)):
-            plt.plot(self.indexes, features[i], marker='x', 
-                     label=self.feature_names()[i])
-        # plt.ylim(0, 1)
+    def plot(self, data_count=10):
+        first_index = self.plot_ds(plt, data_count)
+        # import pdb; pdb.set_trace()
+        index = np.where(self.indexes_hist> first_index)[0][0] - 1
+        opens_hist = self.scaler_opens.transform(self.opens_hist.reshape(-1, 1))
+        plt.plot(
+            self.indexes_hist[index:], 
+            opens_hist[index:], label='open hist')
+        volumes_hist = self.scaler_volumes.transform(
+                                            self.volumes_hist.reshape(-1, 1))
+        plt.plot(
+            self.indexes_hist[index:], 
+            volumes_hist[index:], label='log(volume hist.)')
+        
         plt.legend()
         plt.show()
--- szx81/nn_tools/data_sources.py
+++ szx81/nn_tools/data_sources.py
@@ -1,6 +1,16 @@
 import numpy as np
 np.set_printoptions(formatter={'float_kind':"{:-.3e}".format})
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import matplotlib.pyplot as plt
+
+import logging as lg
+# https://realpython.com/python-logging/
+# https://stackoverflow.com/questions/10973362/python-logging-function-name-file-name-line-number-using-a-single-file
+
+# lg.basicConfig(format="{levelname}:{name}:{message}", style="{")
+lg.basicConfig(format="{levelname}:{funcName}: {message}", style="{")
+log = lg.getLogger(__name__)
+log.setLevel(level=lg.DEBUG)
 
 import core as co
 import hist_data as hd
@@ -24,11 +34,11 @@ class OpenVolumeDs(DataSource):
                 open += (val[1][0][0] + val[1][1][0]) / 2
                 volume += val[2]              
 
-            opens.append(open / self.step) #.reshape(-1, 1)
-            volumes.append(volume / self.step) #.reshape(-1, 1)
+            opens.append(open / self.step)
+            volumes.append(volume / self.step)
 
-        opens = np.array(opens).reshape(-1, 1)
-        volumes = np.log(np.array(volumes).reshape(-1, 1) + 10.0)
+        opens = np.array(opens)
+        volumes = np.log(np.array(volumes) + 10.0)
 
         self.fit_transform([opens, volumes])
 
@@ -47,7 +57,27 @@ class OpenVolumeDs(DataSource):
         opens = []
         volumes = []
         targets = []
-        for i in range(self.begin, self.end_index, self.step):
+        # for i in range(len(self.indexes_hist)):
+        #     index = 0
+        #     open = 0
+        #     volume = 0
+        #     target = 0
+
+        #     for k in range(self.step):
+        #         if k == self.step - 1:
+        #             _open, _volume = helper(i + k)
+        #             open += _open
+        #             volume += _volume
+        #             _open, _volume = helper(i + k + self.future_count)
+        #             target += _open
+        #             index = i + k                    
+        #             indexes.append(index)
+        #             opens.append(open / self.step)
+        #             volumes.append(volume / self.step)
+        #             targets.append(target / self.step)
+
+
+        for i in range(self.begin_index, self.end_index, self.step):
             index = 0
             open = 0
             volume = 0
@@ -66,102 +96,134 @@ class OpenVolumeDs(DataSource):
                     volumes.append(volume / self.step)
                     targets.append(target / self.step)
 
-        opens = np.array(opens).reshape(-1, 1)
-        volumes = np.log(np.array(volumes).reshape(-1, 1) + 10.0)
+        opens = np.array(opens)
+        volumes = self.log_volume(np.array(volumes))
         opens, volumes = self.transform([opens, volumes])
 
-        targets = np.array(targets).reshape(-1, 1)
+        targets = np.array(targets)
         targets = self.transform(targets, index=0)
 
         self.features = np.concatenate((opens, volumes), axis=1)
         self.targets = targets
-        self.indexes = np.array(indexes).reshape(-1, 1)
+        self.indexes = np.array(indexes)
         return (
             self.features, 
             self.targets, 
             self.indexes)
 
-
 class OpenDs(DataSource):
-    def fit_data(self, **kwargs):
-        self.step = kwargs['step']
+
+    def fit_data(self):
+        self.step = self.kwargs['step']
         opens = []
         for i in range(0, len(self.data)):  
             open = 0
-            if not i + self.step < len(self.data):
+            if not i + self.step < len(self.data): 
                 break
             for k in range(self.step):
                 val = self.data[i + k]
                 open += (val[1][0][0] + val[1][1][0]) / 2           
 
-            opens.append(open / self.step) #.reshape(-1, 1)
+            opens.append(open / self.step)
 
-        opens = np.array(opens).reshape(-1, 1)
+        opens = np.array(opens)
 
-        self.fit_transform([opens,])
+        log.debug(f'''
+before transformation
+len(self.data): {len(self.data)}
+len(opens): {len(opens)}
+opens(min. max) = ({np.min(opens):.1e}, {np.max(opens):.1e})
+''')
+
+        opens, = self.fit_transform([opens,])
+
+        log.debug(f'''
+after transformation
+len(self.opens): {len(opens)}                  
+opens(min, max): ({np.min(opens):.1e}, {np.max(opens):.1e})''')
+
 
     def feature_names(self):
         return ('opens',)
     
     def target_names(self):
         return ('opens target',)
+    
+    def raw_data_begin(self):
+        self.feature_data_count = self.data_count * self.step
+        return self.end_index \
+            - (self.feature_data_count + (self.future_count + self.step))
         
     def get_data__(self):
-        def helper(i):
-            val = self.data[i]
-            return (val[1][0][0] + val[1][1][0]) / 2
-
         indexes = [] 
         opens = []
         targets = []
-        for i in range(self.begin, self.end_index, self.step):
-            index = 0
+        opens = []
+        
+        for i in range(self.feature_data_count):
             open = 0
             target = 0
-            
+
             for k in range(self.step):
+                open += self.opens_hist[i + k]
+                target += self.opens_hist[i + k + self.future_count]
                 if k == self.step - 1:
-                    _open = helper(i + k)
-                    open += _open
-                    _open = helper(i + k + self.future_count)
-                    target += _open
-                    index = i + k                    
-                    indexes.append(index)
+                    indexes.append(self.indexes_hist[i + k])
                     opens.append(open / self.step)
                     targets.append(target / self.step)
 
-        opens = np.array(opens).reshape(-1, 1)
-        opens = self.transform([opens,])
+        indexes = np.array(indexes)
+        opens = np.array(opens)
+        opens = self.transform(opens, 0)
+        targets = np.array(targets)
+        targets = self.transform(targets, 0)
 
-        targets = np.array(targets).reshape(-1, 1)
-        targets = self.transform(targets, index=0)
-
+        log.debug(f'''
+len(indexes): {len(indexes)}
+indexes(min. max): ({np.min(indexes)}, {np.max(indexes)})
+len(opens): {len(opens)}
+opens(min. max): ({np.min(opens):.1e}, {np.max(opens):.1e})
+len(targets): {len(targets)}
+targets(min. max): ({np.min(targets):.1e}, {np.max(targets):.1e})
+''')
         self.features = np.concatenate((opens,), axis=1)
         self.targets = targets
-        self.indexes = np.array(indexes).reshape(-1, 1)
+        self.indexes = indexes
         return (
             self.features, 
             self.targets, 
             self.indexes)
+    
+    def plot_ds(self, plt, data_count=10, show=False):
+        targets = self.targets.transpose()
+        for i in range(len(targets)):
+            plt.plot(
+                self.indexes[-data_count:], 
+                targets[i][-data_count:], # marker='o', 
+                     label=self.target_names()[i])
+        features = self.features.transpose()
+        for i in range(len(features)):
+            plt.plot(
+                self.indexes[-data_count:], 
+                features[i][-data_count:], # marker='x', 
+                     label=self.feature_names()[i])
+        if show:
+            plt.legend()
+            plt.show()
+        
+        return self.indexes[-data_count:][0]
 
 def test_ds():
     # ds = OpenVolumeDs(
     #     hd.DICT_DATA.values(), (StandardScaler(), StandardScaler()), step=10)
     ds = OpenDs(
-        hd.DICT_DATA.values(), (MinMaxScaler(), MinMaxScaler()), step=10)
+        hd.DICT_DATA.values(), (MinMaxScaler(), MinMaxScaler()), step=3)
     
-    features, targets, indexes = ds.get_data(
-        end_index=5000, data_count=3000, future_count=3)
+    ds.get_data(end_index=5000, data_count=30, future_count=3)
     ds.report()
+    # ds.plot_ds(plt, data_count=10, show=True)
     ds.plot()
 
-    print(f'''
-    wanted end range: ({1000})
-    index range: ({indexes[0]}, {indexes[-1]})
-    targets.shape: {targets.shape}
-    indexes.shape: {indexes.shape}
-    features.shape: {features.shape}
-    ''')
 
 class ContextSequencer:
     def __init__(self, 
