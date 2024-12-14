feaa1d0b7fd416560b1e9d7456087d16ceb4c7d3
stefanzarembinski <stefan.zarembinski@gmail.com>
2024-12-14 23:26:50 +0100

    update

--- szx81/test.py
+++ szx81/test.py
@@ -1,51 +1,34 @@
-import random
-import math
+from os import path as path
+import core as co
+co.set_test_data_dir('WikiText2')
+co.TEST_DATA_DIR
 
-class Sinus:
-    def __init__(self, noise=0.03, stop=10000, start=0, step=1):
-        self.noise = noise
-        self.start = start
-        self.step = step
-        self.stop = stop 
+train_txt = path.join(co.TEST_DATA_DIR, co.TEST_DATA, 'train.txt')
+test_txt = path.join(co.TEST_DATA_DIR, co.TEST_DATA, 'test.txt')
+data_store = path.join(co.TEST_DATA_DIR, co.TEST_DATA, co.DATA_STORE)
 
-    def __getitem__(self, i):
-        # import pdb; pdb.set_trace()
-        if isinstance(i, slice):
-            if i.start is not None:
-                self.start = i.start
-            if i.step is not None:
-                self.step = i.step
-            if i.stop is not None:
-                self.stop = i.stop
-            return Sinus(self.noise, self.stop, self.start, self.step)
-        if i < 0:
-            i = self.length + i
-        
-        k = ((i + self.start) // self.step) * self.step
-        if k < self.start or k >= self.step:
-            raise IndexError(f'index is {k} while it has to be within limits [{self.start}, {self.stop}[')
-        return .5 * math.sin(k * .03 * self.step) \
-                + random.uniform(-self.noise, self.noise) + .5
+class TrainIter:
+    def __init__(self, data_file):
+        self.tokens = []
+        self.data_file = data_file
+        self.line_iterator = None
     
-    def __len__(self):
-        return self.stop - self.start
-    
-def main():
-    
-    # sin = Sinus(noise=0, len=20)[:3]
-    sin = Sinus(noise=0, stop=20)
-    sin = sin[:18]
-    print(f'''
-len(sin): {len(sin)}
-sin[10] {sin[10]}
-'''
-        )
-    try:
-        sin[21]
-    except Exception as ex:
-        print(ex)
-    sin[0]
+    def __iter__(self):
+        self.file = open(self.data_file, 'r')
+        self.line_iterator = iter(self.file.readline, '')
+        return self
 
+    def __next__(self): # Python 2: def next(self)
+        if len(self.tokens) == 0:
+            while len(self.tokens) == 0:
+                line = next(self.line_iterator)
+                if line == '':
+                    raise StopIteration
+                self.tokens = line.split()
 
-if __name__ == "__main__":
-    main()
+        return self.tokens.pop(0)
+    
+train_iter = iter(TrainIter(train_txt))
+
+for i in range(200):
+    print(next(train_iter))
